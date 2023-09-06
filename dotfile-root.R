84efe30e8b3aab6f55a6b7ee1fcfed172a1c9bce
Vincent Clemson <vclemson07@gmail.com>
2023-09-06 16:59:05 -0400

    gather project root as last output | fixes when there are startup messages from R

--- dotfile-root.R
+++ dotfile-root.R
@@ -2,5 +2,5 @@ dotfile <- '.quarto-version'
 criterion <- rprojroot::root_criterion(
   function(path) file.exists(file.path(path, dotfile)), paste0('has ', dotfile)
 )
-root <- rprojroot::find_root(criterion = criterion, path = '.')
+root <- tryCatch(rprojroot::find_root(criterion = criterion, path = '.'), error = function(e){})
 cat(root)
